40f7a393e288ddaba9c42ea5e06d1c60c20916a4
joseluisgs <joseluisgonsan@gmail.com>
2021-07-24 12:52:41 +0200

    Header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="home">
+    <app-header class="header" v-bind:title="title"></app-header>
     Vue Testing
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Header from '@/components/Header.vue';
 
 export default defineComponent({
   name: 'Home',
+  // Mis componentes
+  components: {
+    'app-header': Header,
+  },
+  // Mis datos
+  data: () => ({
+    title: 'Vue Weather App',
+  }),
 });
 </script>

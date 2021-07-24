1c593fa190bf05aa68c035fd8a453e8a07665529
joseluisgs <joseluisgonsan@gmail.com>
2021-07-24 12:57:20 +0200

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <h1>{{ message }}</h1>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Footer',
+  // Mis propiedades
+  props: {
+    message: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  margin: auto;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <app-header class="header" v-bind:title="title"></app-header>
+    <app-footer class="footer" v-bind:message="footerMessage"></app-footer>
     Vue Testing
   </div>
 </template>
@@ -8,16 +9,19 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 
 export default defineComponent({
   name: 'Home',
   // Mis componentes
   components: {
     'app-header': Header,
+    'app-footer': Footer,
   },
   // Mis datos
   data: () => ({
     title: 'Vue Weather App',
+    footerMessage: 'joseluisgs 2021',
   }),
 });
 </script>

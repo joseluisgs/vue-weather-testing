39a558797962f8eb11cca0634e2bc78eb6b3dd13
joseluisgs <joseluisgonsan@gmail.com>
2021-07-26 13:54:07 +0200

    gh-pages commit

---
+++ precache-manifest.142a4d5552b87b2c109a4a5cbc6ea170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7bbf4435db1c4626a2",
+    "url": "/vue-weather-testing/css/app.c08f68f4.css"
+  },
+  {
+    "revision": "e2e203c86d3d514dbd5e0fce795f90bb",
+    "url": "/vue-weather-testing/index.html"
+  },
+  {
+    "revision": "1b7bbf4435db1c4626a2",
+    "url": "/vue-weather-testing/js/app.e13fadf6.js"
+  },
+  {
+    "revision": "1c906f163521443bf55a",
+    "url": "/vue-weather-testing/js/chunk-vendors.4bb0b571.js"
+  },
+  {
+    "revision": "46bea63b3cbbbe3b2aa1dfd4d29f8bfe",
+    "url": "/vue-weather-testing/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-weather-testing/robots.txt"
+  }
+]);
